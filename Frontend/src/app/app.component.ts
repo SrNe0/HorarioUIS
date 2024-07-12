6cb23cafd162677773f647bd5dc66c5447aed28f
Juan Alarcon <jalarconr7@gmail.com>
2024-07-12 11:28:21 -0500

    Actualizacion de angular y arreglos de maquetacion

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { filter } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet, 
+    RouterLink, 
+    RouterLinkActive, 
+    HeaderComponent,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'FrontHorarioUIS';
+export class AppComponent{
+  
 }
