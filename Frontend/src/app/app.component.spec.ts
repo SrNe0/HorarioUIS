bdde93460e0dcf08f69b5b9f876df6a95b4f2c56
Juan Alarcon <jalarconr7@gmail.com>
2024-07-12 12:21:17 -0500

    Arreglo errores node_modules

--- Frontend/src/app/app.component.spec.ts
+++ Frontend/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'frontHU' title`, () => {
+  it(`should have the 'frontH' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontHU');
+    expect(app.title).toEqual('frontH');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontHU');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontH');
   });
 });
--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,23 +1,13 @@
-import { Component} from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
-import { filter } from 'rxjs';
-
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet, 
-    RouterLink, 
-    RouterLinkActive, 
-    HeaderComponent,
-    CommonModule
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent{
-  
+export class AppComponent {
+  title = 'frontH';
 }
