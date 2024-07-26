2b107a01802b6b2a94ab87d873e6c5986383fbad
Diego Medina <diegosistemas29@gmail.com>
2024-07-26 12:00:05 -0500

    SQL inicial corregido

--- Backend/CSV/horario_uis_complete.sql
+++ Backend/CSV/horario_uis_complete.sql
@@ -117,7 +117,7 @@ DROP TABLE IF EXISTS `edificio`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `edificio` (
-  `idedificio` bigint NOT NULL AUTO_INCREMENT,
+  `id_edificio` bigint NOT NULL AUTO_INCREMENT,
   `nombre` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`idedificio`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
