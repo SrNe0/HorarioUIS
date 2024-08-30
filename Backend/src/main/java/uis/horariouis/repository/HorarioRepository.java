4ae1cbbdc1583f335607930136ed215615f084af
SrNe0 <srne0@Pavilion-Gaming15.tail5e141.ts.net>
2024-08-30 16:36:04 -0500

    Algoritmo genetico funcional V0.1

--- Backend/src/main/java/uis/horariouis/repository/HorarioRepository.java
+++ Backend/src/main/java/uis/horariouis/repository/HorarioRepository.java
@@ -1,12 +1,20 @@
 package uis.horariouis.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import uis.horariouis.model.Grupo;
 import uis.horariouis.model.Horario;
+
+import java.util.List;
 
 @Repository
 public interface HorarioRepository extends JpaRepository<Horario, Long> {
     // Aquí puedes agregar métodos personalizados si necesitas realizar consultas específicas
 
+    @Query("SELECT DISTINCT h.grupo FROM Horario h WHERE h.grupo.asignatura.horasTeoria = 5")
+    List<Grupo> findGruposCon5HorasTeoria();
+
+    List<Horario> findByGrupo(Grupo grupo);
 }
 
